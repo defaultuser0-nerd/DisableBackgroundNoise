d032a5cf0fc321c64563b85d1bf876c8dd7d2762
defaultuser0 <axolotlslmao@gmail.com>
2024-11-30 12:18:11 -0600

    sc upload

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace DisableBackgroundNoise
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+
+		void Start()
+		{
+            HarmonyPatches.ApplyHarmonyPatches();
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/WeatherDayNight/AudioBirds").SetActive(false);
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/WeatherDayNight/AudioSpookyWind").SetActive(false);
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Environment/WeatherDayNight/AudioCrickets").SetActive(false);
+        }
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace DisableBackgroundNoise
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.defaultuser0.DisableBackgroundNoise";
+		public const string Name = "DisableBackgroundNoise";
+		public const string Version = "1.0.0";
+	}
+}
