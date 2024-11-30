5970096229336de96d11c23219505fd843297f30
defaultuser0 <axolotlslmao@gmail.com>
2024-11-30 13:06:56 -0600

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,12 +1,9 @@
 ﻿namespace DisableBackgroundNoise
 {
-	/// <summary>
-	/// This class is used to provide information about your mod to BepInEx.
-	/// </summary>
-	class PluginInfo
-	{
+     class PluginInfo
+     {
 		public const string GUID = "com.defaultuser0.DisableBackgroundNoise";
 		public const string Name = "DisableBackgroundNoise";
 		public const string Version = "1.0.0";
-	}
+     }
 }
