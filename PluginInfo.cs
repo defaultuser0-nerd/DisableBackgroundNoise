546ca57063d0e809756db70299987d2748450d44
defaultuser0 <axolotlslmao@gmail.com>
2024-12-01 10:20:54 -0600

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@
      {
 	 public const string GUID = "com.defaultuser0.DisableBackgroundNoise";
 	 public const string Name = "DisableBackgroundNoise";
-	 public const string Version = "1.0.0";
+	 public const string Version = "1.0.1";
      }
 }
