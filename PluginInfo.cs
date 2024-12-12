13f2c1f4ee7961a9e5dcffdeffce8e57297aabe6
defaultuser0 <axolotlslmao@gmail.com>
2024-12-12 17:48:17 -0600

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,7 +2,7 @@
 {
      class PluginInfo
      {
-	 public const string GUID = "com.defaultuser0.DisableBackgroundNoise";
+	 public const string GUID = "defaultuser0.DisableBackgroundNoise";
 	 public const string Name = "DisableBackgroundNoise";
 	 public const string Version = "1.0.1";
      }
